e38318187dbd7e55fd43e6e674b1ea7fb1f552c6
MathysAM <m.doulcier@am-automation.com>
2025-05-29 19:34:53 +0200

    First commit

---
+++ PizthieR/PizthieR/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PizthieR.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ PizthieR/PizthieR/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PizthieR.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
