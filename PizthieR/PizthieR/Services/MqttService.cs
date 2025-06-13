e7f228e06846ffe27a71872986a8bd7101d7c299
MathysAM <m.doulcier@am-automation.com>
2025-06-13 11:29:05 +0200

    clc

---
+++ PizthieR/PizthieR/Services/MqttService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+
+namespace PizthieR.Services;
+
+public class MqttService
+{
+    private readonly IMqttClient _client;
+
+    public MqttService()
+    {
+        var factory = new MqttFactory();
+        _client = factory.CreateMqttClient();
+    }
+
+    public async Task ConnectAsync(string uri, string username, string password, CancellationToken cancellationToken = default)
+    {
+        var options = new MqttClientOptionsBuilder()
+            .WithClientId(Guid.NewGuid().ToString())
+            .WithWebSocketServer(uri)
+            .WithCredentials(username, password)
+            .WithTls()
+            .Build();
+
+        await _client.ConnectAsync(options, cancellationToken);
+    }
+}
--- PizthieR/PizthieR/Views/MainWindow.axaml.cs
+++ PizthieR/PizthieR/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using PizthieR.Services;
 
 namespace PizthieR.Views;
 
@@ -7,5 +8,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        // Example usage of the MQTT service on startup
+        var mqttService = new MqttService();
+        _ = mqttService.ConnectAsync(
+            "wss://4d1f194df18748a393eeabb274d5e439.s1.eu.hivemq.cloud:8884/mqtt",
+            "MathysChef",
+            "Flth2112");
     }
 }
