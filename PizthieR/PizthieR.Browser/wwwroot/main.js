ad3dc2d38a5bf466037e8ae0673d58dd2dc9cc67
MathysAM <m.doulcier@am-automation.com>
2025-08-23 21:39:09 +0200

    ok

--- PizthieR/PizthieR.Browser/wwwroot/main.js
+++ PizthieR/PizthieR.Browser/wwwroot/main.js
@@ -1,20 +1,22 @@
-import { dotnet } from './_framework/dotnet.js'
+import { dotnet } from './_framework/dotnet.js';
 
-const is_browser = typeof window != "undefined";
-if (!is_browser) throw new Error(`Expected to be running in a browser`);
+const is_browser = typeof window !== 'undefined';
+if (!is_browser) throw new Error('Expected to be running in a browser');
 
-const dotnetRuntime = await dotnet
+const runtime = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
 
-const config = dotnetRuntime.getConfig();
+const config = runtime.getConfig();
 
-// Lance l'app Avalonia
-await dotnetRuntime.runMain(config.mainAssemblyName, [globalThis.location.href]);
+// Lance Avalonia
+await runtime.runMain(config.mainAssemblyName, [globalThis.location.href]);
 
-// Quand Avalonia est montée, on supprime le splash custom
-const splash = document.getElementById("splash");
-if (splash) {
-    splash.remove();
-}
+// Supprime le splash personnalisé
+const splash = document.getElementById('splash');
+if (splash) splash.remove();
+
+// Supprime au cas où le splash Avalonia par défaut serait injecté
+const injected = document.querySelector('.avalonia-splash');
+if (injected) injected.remove();
