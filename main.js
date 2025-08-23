c0744a995fd1b1bfd97139411a00f50caeb82e44
MathysAM <MathysAM@users.noreply.github.com>
2025-08-23 19:41:34 +0000

    deploy: ad3dc2d38a5bf466037e8ae0673d58dd2dc9cc67

---
+++ main.js
@@ -0,0 +1,22 @@
+import { dotnet } from './_framework/dotnet.js';
+
+const is_browser = typeof window !== 'undefined';
+if (!is_browser) throw new Error('Expected to be running in a browser');
+
+const runtime = await dotnet
+    .withDiagnosticTracing(false)
+    .withApplicationArgumentsFromQuery()
+    .create();
+
+const config = runtime.getConfig();
+
+// Lance Avalonia
+await runtime.runMain(config.mainAssemblyName, [globalThis.location.href]);
+
+// Supprime le splash personnalisé
+const splash = document.getElementById('splash');
+if (splash) splash.remove();
+
+// Supprime au cas où le splash Avalonia par défaut serait injecté
+const injected = document.querySelector('.avalonia-splash');
+if (injected) injected.remove();
