650e4583c5952730c0c2b8f2d4c54fc17e0a9a1f
MathysAM <m.doulcier@am-automation.com>
2025-12-18 16:41:12 +0100

    modif

--- PizthieR/PizthieR.Browser/Program.cs
+++ PizthieR/PizthieR.Browser/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection.PortableExecutable;
+﻿using System;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Browser;
@@ -6,10 +6,21 @@ using PizthieR;
 
 internal sealed partial class Program
 {
-    private static Task Main(string[] args) =>
-        BuildAvaloniaApp()
-            .WithInterFont()
-            .StartBrowserAppAsync("out");
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            await BuildAvaloniaApp()
+                .WithInterFont()
+                .StartBrowserAppAsync("out");
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine("=== Avalonia startup failed ===");
+            Console.Error.WriteLine(ex.ToString()); // IMPORTANT: imprime inner exceptions
+            throw;
+        }
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>();
